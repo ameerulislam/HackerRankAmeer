b2eeb11ec46fccf0a7fb46ebf654b27f6954958c
Ameer <ameerulislam@gmail.com>
2021-12-04 00:10:46 +0600

    morning of 4 Dec

--- PracticeDataStructuresArrays2DArray - DS.py
+++ PracticeDataStructuresArrays2DArray - DS.py
@@ -1,11 +1,11 @@
 import itertools
 # 6 X 6 Metrics
-list2d = [[1, 2, 3, 4, 5, 6],
-            [1, 1, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0, 0],
-            [0, 0, 2, 4, 4, 0],
-            [0, 0, 0, 2, 0, 0],
-            [0, 0, 1, 2, 4, 0]]
+list2d = [[1, 2, 3, 4, 5, 6], #head only
+            [1, 1, 1, 0, 0, 0], #head only and body
+            [1, 1, 1, 0, 0, 0], #both head , tail and body
+            [0, 0, 2, 4, 4, 0], #both head , tail and body
+            [0, 0, 0, 2, 0, 0], #tail and body
+            [0, 0, 1, 2, 4, 0]] #tail only
 # check if the metrics is square oterwise we're not playing the glass game, we might be able to but probably square is easier        
 def squaremetrix(arr):
     listrows = len(arr) #number of rows
@@ -25,11 +25,15 @@ def glasshours(arr):
     if squaremetrix(list2d)[0] == 'list is a square':
         matrixShape = [squaremetrix(list2d)[1] , squaremetrix(list2d)[1]]
         # print("matrix shape ",matrixShape)
-        for i in arr:
-            # lengthofi = len(i)
-            # howmanyheads = (lengthofi/3) + 2 #length of row divide by head size + 2
-            combinations = itertools.combinations(i, 3)
-            print(list(combinations))
+        
+        for index, i in enumerate (arr):
+            lengthofi = len(i)
+            howmanyheads = int(lengthofi/3) + 2 #length of row divide by head size + 2
+            headsNtailes = list(itertools.repeat(i, howmanyheads)) #repeat the same item the number of heads we need
+            counter = -1
+            for j in headsNtailes:
+                print(j[counter+1:counter+4])
+                counter += 1
                      
 
 glasshours(list2d)
