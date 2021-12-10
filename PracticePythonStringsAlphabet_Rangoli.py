b7c301bf50d0224f649248c271b033d64c32c22d
Ameer <ameerulislam@gmail.com>
2021-12-10 23:32:26 +0600

    day end 10 Dec

---
+++ PracticePythonStringsAlphabet_Rangoli.py
@@ -0,0 +1,4 @@
+import string
+
+letter_list = [i for i in string.ascii_lowercase]
+print(letter_list)
---
+++ Problem Solving with Python/permutations_and_computations.py
@@ -0,0 +1,7 @@
+import itertools
+
+
+list1 = [1,2,3] 
+
+print(itertools.permutations(list1, 2))
+print(itertools.combinations(list1, 2))
